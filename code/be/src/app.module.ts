245989d0d2a36af10836660525c8c7e52730bbe9
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-02-27 23:14:22 +0700

    feat: add config database

--- code/be/src/app.module.ts
+++ code/be/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from 'src/config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        port: configService.get<number>('database.port'),
+        host: configService.get<string>('database.host'),
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/database/migrations/*.js'],
+        synchronize: false,
+        logging: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
