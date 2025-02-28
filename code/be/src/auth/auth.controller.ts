6eb369f5e6fa3fd1e05ccb324e834153d6c8a02d
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-02-28 19:38:43 +0700

    feat: enable CORS and return only accessToken & refreshToken on sign-in

--- code/be/src/auth/auth.controller.ts
+++ code/be/src/auth/auth.controller.ts
@@ -1,22 +1,30 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { SESSION_MAX_AGE } from 'src/libs/common/constants';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
-import { UserSession } from 'src/libs/common/decorators';
+import { Roles, UserSession } from 'src/libs/common/decorators';
 import { TUserSession } from 'src/libs/common/types';
+import { JwtAuthGuard, RoleAuthGuard } from 'src/auth/guards';
+import { Role } from 'src/users/enums/role.enum';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Post('sign-up')
   async signUp(@Body() signUpDto: SignUpDto) {
@@ -39,11 +47,23 @@ export class AuthController {
       expired_at: new Date(Date.now() + SESSION_MAX_AGE),
     };
 
-    return response;
+    return {
+      access_token: data.access_token,
+      refresh_token: data.refresh_token,
+    };
   }
 
   @Post('refresh-token')
   refreshAccessToken(@UserSession() userSession: TUserSession) {
     return this.authService.handleRefreshToken(userSession);
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard, RoleAuthGuard)
+  @Roles(Role.ADMIN, Role.USER)
+  async handleGetProfile(@UserSession() userSession: TUserSession) {
+    const { userId } = userSession;
+
+    return this.usersService.handleGetProfileByUserId(userId);
   }
 }
