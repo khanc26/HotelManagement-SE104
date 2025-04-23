b17951ccb92f83ced1fd53970707b878c85d2551
Bao Le <levanbao2105@gmail.com>
2025-04-23 21:08:18 +0700

    feat: add configurations controller and update DTO for configuration management

--- code/be/src/modules/configurations/configurations.service.ts
+++ code/be/src/modules/configurations/configurations.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Configuration } from 'src/modules/configurations/entities';
 import { Repository } from 'typeorm';
+import { UpdateConfigurationDto } from './dto/update-configuration.dto';
 
 @Injectable()
 export class ConfigurationsService {
@@ -14,5 +15,43 @@ export class ConfigurationsService {
     return await this.configurationRepository.findOne({
       where: { configName },
     });
+  };
+
+  getAllConfigurations = async () => {
+    return await this.configurationRepository.find({
+      withDeleted: true,
+      order: {
+        createdAt: 'DESC',
+      },
+    });
+  };
+
+  updateConfiguration = async (
+    configName: string,
+    updateConfigurationDto: UpdateConfigurationDto,
+  ) => {
+    const existingConfig = await this.configurationRepository.findOne({
+      where: {
+        configName,
+      },
+    });
+
+    if (!existingConfig) {
+      throw new NotFoundException(
+        `Configuration with name ${configName} not found`,
+      );
+    }
+
+    const { id, ...existingConfigValue } = existingConfig;
+    const newConfigValue = this.configurationRepository.create({
+      ...existingConfigValue,
+      ...updateConfigurationDto,
+    });
+
+    await this.configurationRepository.softRemove(existingConfig);
+    await this.configurationRepository.save(newConfigValue);
+
+    const configurations = await this.getAllConfigurations();
+    return configurations;
   };
 }
