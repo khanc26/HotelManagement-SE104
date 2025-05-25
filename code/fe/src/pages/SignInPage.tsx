90a36a7ac524b795ffb6657ba9955f5da256a5c5
khanc26 <khaqt268@gmail.com>
2025-05-25 15:50:18 +0700

    fix: fix merge bugs

--- code/fe/src/pages/SignInPage.tsx
+++ code/fe/src/pages/SignInPage.tsx
@@ -65,104 +65,95 @@ const SignInPage = () => {
       });
     } catch (err: any) {
       console.error(err);
-      toast.success("Signed in successfully!", {
+      toast.error("Failed to sign in. Please check your credentials.", {
         position: "bottom-right",
       });
     }
-
-    return (
-      <div className="mx-auto md:px-6 px-2 flex flex-col md:gap-6 gap-3">
-        <div className="flex flex-col text-center items-center">
-          <h2 className="text-2xl font-bold text-center">SIGN IN</h2>
-          <p className="text-gray-600 text-center">
-            Sign in to manage reservations, guests, and hotel services
-            efficiently.
-          </p>
-        </div>
-
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col md:gap-4 gap-2 md:w-2/3 w-full mx-auto"
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-black">Email</FormLabel>
-                  <FormControl>
-                    <Input {...field} placeholder="johndoe01@gmail.com" />
-                  </FormControl>
-                  <FormMessage className="text-red-600" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Your password here..."
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage className="text-red-600" />
-                </FormItem>
-              )}
-            />
-
-            <div className="flex items-center justify-between">
-              <p>
-                <Link
-                  to="/auth/forgot-password"
-                  className="hover:text-blue-600 transition-all hover:underline text-sm"
-                >
-                  Forgot your password?
-                </Link>
-              </p>
-
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label htmlFor="terms" className="text-sm select-none">
-                  Remember me
-                </label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label htmlFor="terms" className="text-sm select-none">
-                  Remember me
-                </label>
-              </div>
-            </div>
-
-            <Button type="submit" className="w-fit mx-auto" color="primary">
-              Sign In
-            </Button>
-            <Button type="submit" className="w-fit mx-auto" color="primary">
-              Sign In
-            </Button>
-
-            <div className="space-y-2 text-center text-sm">
-              <p>
-                Don't have an account?{" "}
-                <Link
-                  to="/auth/sign-up"
-                  className="text-blue-600 hover:underline"
-                >
-                  Sign up
-                </Link>
-              </p>
-            </div>
-          </form>
-        </Form>
-      </div>
-    );
   }
+
+  return (
+    <div className="mx-auto md:px-6 px-2 flex flex-col md:gap-6 gap-3">
+      <div className="flex flex-col text-center items-center">
+        <h2 className="text-2xl font-bold text-center">SIGN IN</h2>
+        <p className="text-gray-600 text-center">
+          Sign in to manage reservations, guests, and hotel services
+          efficiently.
+        </p>
+      </div>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col md:gap-4 gap-2 md:w-2/3 w-full mx-auto"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-black">Email</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="johndoe01@gmail.com" />
+                </FormControl>
+                <FormMessage className="text-red-600" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Your password here..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="text-red-600" />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex items-center justify-between">
+            <p>
+              <Link
+                to="/auth/forgot-password"
+                className="hover:text-blue-600 transition-all hover:underline text-sm"
+              >
+                Forgot your password?
+              </Link>
+            </p>
+
+            <div className="flex items-center space-x-2">
+              <Checkbox id="terms" />
+              <label htmlFor="terms" className="text-sm select-none">
+                Remember me
+              </label>
+            </div>
+          </div>
+
+          <Button type="submit" className="w-fit mx-auto" color="primary">
+            Sign In
+          </Button>
+
+          <div className="space-y-2 text-center text-sm">
+            <p>
+              Don't have an account?{" "}
+              <Link
+                to="/auth/sign-up"
+                className="text-blue-600 hover:underline"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default SignInPage;
